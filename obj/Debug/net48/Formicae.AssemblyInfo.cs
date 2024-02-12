7ba5845d68c66ba3a59d9fc2b40ecd9234400ba5
Mikael Spinnangr <89906534+MikaelSpinnangr@users.noreply.github.com>
2024-02-12 18:56:12 +0100

    First commit

--- obj/Debug/net48/Formicae.AssemblyInfo.cs
+++ obj/Debug/net48/Formicae.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of Formicae")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+98798c8d0204d38ff87b0f308683bbe9d56c5c91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Formicae")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Formicae")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
