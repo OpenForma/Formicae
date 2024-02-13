a1950a430ee11a53fd003162dbe7064a340293c2
Mikael Spinnangr <89906534+MikaelSpinnangr@users.noreply.github.com>
2024-02-13 19:22:34 +0100

    GH_CreateSIMBox

---
+++ Components/GH_CreateSimulationBox.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Formicae.Types;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Formicae.Components
+{
+    public class GH_CreateSimulationBox : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GH_CreateSimulationBox class.
+        /// </summary>
+        public GH_CreateSimulationBox()
+          : base("Create Simulation Box ", "Nickname",
+              "Description",
+              "Formicae", "Setup")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBrepParameter("Box", "box", "500X500 box to make the simulaiton Grids", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("SimulaitonMeshGrid", "SMG", "A grid to simulate on", GH_ParamAccess.item);
+            pManager.AddMeshParameter("ResultMeshGrid", "RMG", "A grid to viz the results",GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Brep box = new Brep();
+            DA.GetData(0, ref box);
+
+            SimulationBox simbox = new SimulationBox(box);
+            var simMesh = simbox.GetSimulationMesh();
+            var ResultMesh = simbox.GetResultMeshGrid();
+
+            DA.SetData(0, simMesh);
+            DA.SetData(1, ResultMesh);
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("1F1E5EE5-6DD9-436C-9C7B-4D51AD92B912"); }
+        }
+    }
+}
