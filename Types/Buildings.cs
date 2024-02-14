af6c7281f50023a638acc31693cb4e4592fd9d02
Mikael Spinnangr <89906534+MikaelSpinnangr@users.noreply.github.com>
2024-02-14 02:28:26 +0100

    Building to Buildings

---
+++ Types/Buildings.cs
@@ -0,0 +1,47 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formicae.Types
+{
+    public class Buildings
+    {
+        public Brep buildingBreps;
+
+        public Buildings() { }
+        
+        public Mesh previewMesh => GetBuildingsPreviewMesh();
+
+        public Buildings(Brep brep) 
+        {
+            buildingBreps = brep;
+        }
+
+        public Buildings(List<Brep>breps)
+        {
+            Brep brep = new Brep();
+            foreach(var b in breps) 
+            {
+                brep.Append(b);
+            }
+            buildingBreps = brep;
+        }
+
+        public Mesh GetBuildingsPreviewMesh() 
+        {
+            Mesh mesh = new Mesh();
+            Mesh[] meshes = Mesh.CreateFromBrep(this.buildingBreps, MeshingParameters.FastRenderMesh);
+            foreach (var m in meshes)
+            {
+                mesh.Append(m);
+            }
+            return mesh;
+        }
+        
+
+    }
+}
