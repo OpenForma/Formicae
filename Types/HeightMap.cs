1e796132fe0687addcac2858434db682298d30d1
Mikael Spinnangr <89906534+MikaelSpinnangr@users.noreply.github.com>
2024-02-14 02:28:50 +0100

    Create WindModel and HeightMaps

---
+++ Types/HeightMap.cs
@@ -0,0 +1,46 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry.Intersect;
+using Formicae.Helpers;
+
+namespace Formicae.Types
+{
+    public class HeightMap
+    {
+        public WindSimulationModel Model { get; set; }  
+
+        public HeightMap() { }  
+
+        public HeightMap(WindSimulationModel model) 
+        {
+            this.Model = model; 
+        }
+
+        public List<double> HeightMapWithoutBuildings()
+        {
+            List<double> heights = new List<double>();
+           List<Point3d> projectedPts = MeshHelper.ProjectPointsDownardOnMesh(Model.SimulationBox.LiftedPts, Model.GetModelMeshWithoutBuildings());
+            foreach (Point3d p in projectedPts)
+            {
+                heights.Add(p.Z);
+            }
+            return heights;
+        }
+
+        public List <double> HeightMapWithBuildings() 
+        {
+            List<double> heights = new List<double>();
+            List<Point3d> projectedPts = MeshHelper.ProjectPointsDownardOnMesh(Model.SimulationBox.LiftedPts, Model.GetModelMeshWithBuildings());
+            foreach (Point3d p in projectedPts)
+            {
+                heights.Add(p.Z);
+            }
+            return heights;
+        }
+    }
+}
---
+++ Types/WindSimulationModel.cs
@@ -0,0 +1,53 @@
+﻿using Formicae.Helpers;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formicae.Types
+{
+    public class WindSimulationModel
+    {
+        WindSimulationModel() { }
+        
+        public Buildings Buildings { get; set; } 
+        public Terrain Terrain { get; set; }
+        public SimulationBox SimulationBox { get; set; }
+
+        public Mesh ModelWithBuildings => GetModelMeshWithBuildings();
+        public Mesh ModelwithoutBuildings => GetModelMeshWithoutBuildings();
+
+        public Mesh DrapedResultMesh => GetResultMeshDraped();
+
+        WindSimulationModel(Buildings blgs , Terrain terrain, SimulationBox simBox) 
+        {
+            this.Buildings = blgs;
+            this.Terrain = terrain;
+            this.SimulationBox = simBox;
+        }
+
+        public Mesh GetModelMeshWithBuildings()
+        {
+            Mesh mesh = new Mesh();
+            mesh.Append(Buildings.previewMesh);
+            mesh.Append(Terrain.terrainMesh);
+            return mesh;
+        }
+
+        public Mesh GetModelMeshWithoutBuildings() 
+        {
+            Mesh mesh = new Mesh(); 
+            mesh.Append(Terrain.terrainMesh);
+            return mesh;
+        }
+
+        public Mesh GetResultMeshDraped()
+        {
+            return MeshHelper.DrapeMesh(this.SimulationBox.LiftedResultMesh, this.Terrain.terrainMesh);
+        }
+       
+
+    }
+}
